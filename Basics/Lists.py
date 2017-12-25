9eb4ec484183b6c79d135961ef364a437c354e59
Pavlo Shchur <pavloshchur@gmail.com>
2017-12-25 21:03:13 +0200

    Learn about tuples

--- Basics/Lists.py
+++ Basics/Lists.py
@@ -3,3 +3,7 @@ tinyList = [123, 'john']
 
 print('list :', list)  # Prints complete list
 print('list[0] : ', list[0])  # Prints first element of the list
+print('list[1:3] : ', list[1:3])  # Prints elements starting from 2nd till 3rd
+print('list[2:] : ', list[2:])  # Prints elements starting from 3rd element
+print('tinyList * 2 : ', tinyList * 2)  # Prints list two times
+print('tinyList + list : ', tinyList + list)  # Prints concatenated lists
