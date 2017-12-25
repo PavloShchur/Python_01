c383dda3905d5c09bb443ddac9d214465f4b478f
Pavlo Shchur <pavloshchur@gmail.com>
2017-12-25 20:56:50 +0200

    Commit changes

--- Basics/Lists.py
+++ Basics/Lists.py
@@ -2,4 +2,4 @@ list = ['abcd', 786, 2.23, 'john', 70.200000000000]
 tinyList = [123, 'john']
 
 print('list :', list)  # Prints complete list
-print()
+print('list[0] : ', list[0])  # Prints first element of the list
