61423c0f704e66b673f4be5659b504ad3e726737
Pavlo Shchur <pavloshchur@gmail.com>
2017-12-25 20:50:57 +0200

    First commit

---
+++ Basics/Lists.py
@@ -0,0 +1,5 @@
+list = ['abcd', 786, 2.23, 'john', 70.200000000000]
+tinyList = [123, 'john']
+
+print('list :', list)  # Prints complete list
+print()
---
+++ Basics/variableTypes.py
@@ -0,0 +1,34 @@
+from builtins import print
+
+counter = 100  # An integer assignment
+miles = 1000.0  # A floating point
+name = "John"  # A string
+
+print(counter)
+print(miles)
+print(name)
+
+a, b, c = 1, 2, "John"
+print('a : ', a)
+print('b : ', b)
+print('c : ', c)
+
+var1 = 10
+var2 = 20
+
+print('var1 : ', var1)
+print('var2 : ', var2)
+
+# del var1
+# print('var1 : ', var1)
+
+print('////////////////////')
+
+str = 'Hello World!'
+
+print('str : ', str)  # Prints complete string
+print('str : ', str[0])  # Prints first character ofv the string
+print('str : ', str[2:5])  # Prints characters starting from 3rd to 5th
+print('str : ', str[2:])  # Prints string starting from 3rd character
+print('str : ', str * 2)  # Prints string two times
+print('str : ', str + " TEST")  # Prints concatenated strin
